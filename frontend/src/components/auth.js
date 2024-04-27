119bb7404214fa8b8200765019e6569a853436de
luisdrg <luisdrg@email.unc.edu>
2024-04-27 13:09:53 -0400

    Add sign in option

--- frontend/src/components/auth.js
+++ frontend/src/components/auth.js
@@ -1,21 +1,38 @@
 import { auth } from "../config/firebase";
-import {createUserWithEmailAndPassword, signOut} from "firebase/auth";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from "firebase/auth";
 import {useState} from "react";
 
 function Auth() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const signIn = async () => {
+    console.log(auth?.currentUser?.email)
+
+    const signUp = async () => {
         try{
             await createUserWithEmailAndPassword(auth, email, password);
+
+            //To-do: Create new user in firestore databse.
+
         }catch(err){
+            if (err.code === 'auth/email-already-in-use'){
+                alert('Email already in use. Please log in or recover your password.');
+            }
+            console.error(err);
+        }
+    }
+
+    const signIn = async () => {
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+        } catch (err) {
             console.error(err);
         }
     }
 
     const logOut = async () => {
         try{
+            alert(`Logging out: ${email}`)
             await signOut(auth);
         }catch(err){
             console.error(err);
@@ -28,6 +45,7 @@ function Auth() {
         <input placeholder = "Password..."
         type="password"
         onChange={(e)=>setPassword(e.target.value)}></input>
+        <button onClick={signUp}>Create Account</button>
         <button onClick={signIn}> Sign In</button>
         <button onClick={logOut}>Log Out</button>
     </div>
