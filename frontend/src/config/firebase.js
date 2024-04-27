86e9f6fc574e631d1b7cc7b4ee11b85aabed5508
luisdrg <luisdrg@email.unc.edu>
2024-04-26 22:07:00 -0400

    Set up authentication with Firebase

---
+++ frontend/src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBt8KDfOUoTwlng9EB4cxuqjHZBFrm68IY",
+  authDomain: "final-project-15beb.firebaseapp.com",
+  projectId: "final-project-15beb",
+  storageBucket: "final-project-15beb.appspot.com",
+  messagingSenderId: "447162441122",
+  appId: "1:447162441122:web:be3751767f9727da50e6fb",
+  measurementId: "G-BTJTKQE0ZF"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
