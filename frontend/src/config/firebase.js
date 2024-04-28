f3c79d6cfe889a6bc2501d6e2cb16f51709ee6cf
luisdrg <luisdrg@email.unc.edu>
2024-04-28 17:31:04 -0400

    Initiliaze firebase database

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth} from "firebase/auth"
-
+import { getFirestore } from "firebase/firestore"
 const firebaseConfig = {
   apiKey: "AIzaSyBt8KDfOUoTwlng9EB4cxuqjHZBFrm68IY",
   authDomain: "final-project-15beb.firebaseapp.com",
@@ -13,3 +13,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+export const db =  getFirestore(app);
